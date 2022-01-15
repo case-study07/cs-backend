faba8d89de24daaee8c32217e864da2f83bcec86
yusuke <38mtyuusuuke@gmail.com>
2022-01-15 15:33:24 +0900

    carEntry作成

---
+++ pages/admincarlist.tsx
@@ -0,0 +1,203 @@
+export default function adminCarList() {
+    return (
+        <div>
+                    <article>
+            <h2 hidden>車登録</h2>
+            <p>アイコン</p>
+            <form action="">
+                <input type="file" name="" id="" />
+                <input type="file" name="" id="" />
+                <input type="file" name="" id="" />
+                <input type="file" name="" id="" />
+                <input type="file" name="" id="" />
+                <section>
+                    <h3>車詳細</h3>
+                    <dl>
+                        <dt>型式</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>形状</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>外装色</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>内装色</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>シフト</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+                        
+                        <dt>車台番号</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>駆動方式</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>排気量</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+                        
+                        <dt>エンジン種別</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+                        
+                        <dt>輸入車</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+                        
+                        <dt>評価点</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>車体評価</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>メーター交換</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>車体評価</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+                        
+                        <dt>外装評価</dt>
+                        <dd>A</dd>
+                        
+                        <dt>評価</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+                        
+                        <dt>駆動方式</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+                        
+                        <dt>ハンドル</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>ギア</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>エアコン</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+                    
+                        <dt> 乗車人数</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+                    
+                        <dt>ドア数</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>車歴</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>付加</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>色替</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+    
+                        <dt>年式(初年登録年)</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+
+                        <dt>走行距離</dt>
+                        <dd>
+                            <input type="text" name="" id="" />
+                            </dd>
+                        
+                        <dt>修復歴</dt>
+                        <dd>
+                            <input type="radio" name="" id="" />あり</dd>
+                        <dd>
+                            <input type="radio" name="" id="" />なし</dd>
+    
+                        <dt>禁煙車</dt>
+                        <dd>
+                            <input type="radio" name="" id="" />あり</dd>
+                        <dd>
+                            <input type="radio" name="" id="" />なし</dd>                        
+                        
+                        <dt>リサイクル</dt>
+                        <dd>
+                            <input type="radio" name="" id="" />あり</dd>
+                        <dd>
+                            <input type="radio" name="" id="" />なし</dd>                        
+                        
+                        <dt>キャンピングカー</dt>
+                        <dd>
+                            <input type="radio" name="" id="" />あり</dd>
+                        <dd>
+                            <input type="radio" name="" id="" />なし</dd>    
+                        
+                        <dt>福祉車両</dt>
+                        <dd>
+                            <input type="radio" name="" id="" />あり</dd>
+                        <dd>
+                            <input type="radio" name="" id="" />なし</dd>    
+                        
+                        <dt>正規輸入車</dt>
+                        <dd>
+                            <input type="radio" name="" id="" />あり</dd>
+                        <dd>
+                            <input type="radio" name="" id="" />なし</dd>                        
+                        
+                        <dt>車検</dt>
+                        <dd><textarea name="" id="" cols={30} rows={10}></textarea></dd>
+                       
+                        <dt>法廷整備</dt>
+                        <dd><textarea name="" id="" cols={30} rows={10}></textarea></dd>
+                       
+                        <dt>保障</dt>
+                        <dd><textarea name="" id="" cols={30} rows={10}></textarea></dd>
+                       
+                        
+                </dl>
+                </section>
+                <input type="submit" value="登録" />
+            </form>
+
+        </article>
+        </div>
+    )
+}
