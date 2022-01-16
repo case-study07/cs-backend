3cde22bce43befe1cf18a07deff8de76aa18ad02
yusuke <38mtyuusuuke@gmail.com>
2022-01-16 16:26:27 +0900

    indexHTML＆cssのcategory欄を修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         "footer-color": "#FFB9B9",
         "main-color": "#E8E7E3",
         "link-color": "#4A6EEF",
+        "category-color": "#E5E8EF",
       },
     },
   },
