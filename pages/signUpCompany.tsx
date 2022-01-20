acf53d24162cf111cf3c9bd90fe8cf708cf80580
hiroseriku <75381119+hiroseriku@users.noreply.github.com>
2022-01-20 10:42:21 +0900

    mock置き換えcss(signupCompany)

--- pages/signUpCompany.tsx
+++ pages/signUpCompany.tsx
@@ -1,44 +1,41 @@
 import { Layout } from "components/ui";
+import s from "../styles/signUpCompany.module.css";
 
 export default function signUpCompany() {
     return (
-        <>
-                  <article>
-            <h2 hidden>サインアップ（企業）</h2>
-            <form action="">
-                <div>
-                    <label htmlFor="" hidden>アイコン</label>
-                    <input type="file" name="" id=""/>
-                </div>
-                <div>
-                    <label htmlFor="">名前</label>
-                    <input type="text" name="" id=""/>
-                </div>
-                <div>
-                    <label htmlFor="">企業名</label>
-                    <input type="text" name="" id=""/>
-                </div>
-                <div>
-                    <label htmlFor="">メールアドレス</label>
-                    <input type="email" name="" id=""/>
-                </div>
-                <div>
-                    <label htmlFor="">パスワード</label>
-                    <input type="password" name="" id=""/>
-                </div>
-                <div>
-                    <label htmlFor="">確認用パスワード</label>
-                    <input type="password" name="" id=""/>
-                </div>
-                <div>
-                    <label htmlFor="">電話番号</label>
-                    <input type="tel" name="" id=""/>
-                </div>
+      <>
+        <article className={s.signUpCompany}>
+          <h2>サインアップ（企業）</h2>
+          <form action="">
+            <div>
+              <label htmlFor="">名前</label><br />
+              <input type="text" name="" id="" />
+            </div>
+            <div>
+              <label htmlFor="">企業名</label><br />
+              <input type="text" name="" id="" />
+            </div>
+            <div>
+              <label htmlFor="">メールアドレス</label><br />
+              <input type="email" name="" id="" />
+            </div>
+            <div>
+              <label htmlFor="">パスワード</label><br />
+              <input type="password" name="" id="" />
+            </div>
+            <div>
+              <label htmlFor="">確認用パスワード</label><br />
+              <input type="password" name="" id="" />
+            </div>
+            <div>
+              <label htmlFor="">電話番号</label><br />
+              <input type="tel" name="" id="" />
+            </div>
 
-                <input type="submit" value="新規作成" />
-            </form>
+            <input className={s.submit} type="submit" value="新規作成" />
+          </form>
         </article>
-        </>
-    )
+      </>
+    );
 }
 signUpCompany.Layout = Layout;
